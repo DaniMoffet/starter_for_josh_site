bd27ca7b7b3781d5592cedb2af63ab71c7da59d1
DaniMoffet <80152728+DaniMoffet@users.noreply.github.com>
2021-03-13 19:19:49 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="container-fluid"> <!-- div for the main image to streth to the edge of the page-->
+        
+        <div class="container"> <!--  div to add content in the middle of the page-->
+            
+            <div class="row"> <!--  row gives asset to column-->
+                
+                <div class="col-md-6 text-center footer-content">
+                   <p> <?php bloginfo('name'); ?></p>
+                    <img src="<img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+" alt="tumblr">
+                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt="rss">
+                         
+                </div> <!-- column-->
+                
+                 <div class="col-md-6 text-center footer-content">
+                    <p> Terms and Conditions</p>
+                     <img src="Images/linkedin.jpg" alt="linkedin">
+                     <img src="Images/facebook.jpg" alt="facebook">
+                </div> <!-- column-->
+                
+                
+            
+            </div> <!-- row-->
+        
+        </div> <!-- container-->
+    
+    </footer> <!-- container-fluid-->
+
+</body>
+
+</html>
+<?php get_footer(); ?>
